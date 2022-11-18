2180b68deb8bbb5c498b35081c0437675d24f54f
expertglacier <118358272+expertglacier@users.noreply.github.com>
2022-11-18 14:26:24 +0700

    submit homeworks

---
+++ map_exercise/bin/map_exercise.dart
@@ -0,0 +1,11 @@
+void main() {
+    Map info = {
+        "Name": "Nguyễn Văn A",
+        "Age": 15,
+        "Sex": "Nam",
+        "Interests": "Ăn"
+    };
+
+    info["Age"] = 17;
+    info["Interests"] = "Uống";
+}
---
+++ map_exercise/test/map_exercise_test.dart
@@ -0,0 +1,8 @@
+import 'package:map_exercise/map_exercise.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
